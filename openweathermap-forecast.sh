53e849a68d12a5d43602ae925d3bfc49348bdce2
Jean-Luc Tambasco <an.obscurity@gmail.com>
2018-09-11 17:28:18 +1000

    Updated trend symbol on weather app.

--- openweathermap-forecast.sh
+++ openweathermap-forecast.sh
@@ -28,6 +28,7 @@ KEY="23a2c2a7dc0013ee24a45f4eae4bb548"
 CITY=""
 UNITS="metric"
 SYMBOL="°C"
+MATERIAL_FONT_IDX=2 # Index of the material icons font in the Polybar config file.
 WEATHER_FONT_IDX=3 # Index of the weather icons font in the Polybar config file.
 
 until wget -q -O - http://www.google.com | grep Lucky > /dev/null; do
@@ -57,12 +58,12 @@ if [ ! -z "$current" ] && [ ! -z "$forecast" ]; then
     forecast_icon=$(echo "$forecast" | jq -r ".list[].weather[0].icon")
 
     if [ "$current_temp" -gt "$forecast_temp" ]; then
-        trend=""
+        trend="%{T$MATERIAL_FONT_IDX}%{T-}"
     elif [ "$forecast_temp" -gt "$current_temp" ]; then
-        trend=""
+        trend="%{T$MATERIAL_FONT_IDX}%{T-}"
     else
-        trend=""
+        trend="%{T$MATERIAL_FONT_IDX}%{T-}"
     fi
 
-    echo "%{T$WEATHER_FONT_IDX}$(get_icon "$current_icon"%{T-}) $current_temp$SYMBOL  %{T$WEATHER_FONT_IDX}$trend  $(get_icon "$forecast_icon")%{T-} $forecast_temp$SYMBOL"
+    echo "%{T$WEATHER_FONT_IDX}$(get_icon "$current_icon"%{T-}) $current_temp$SYMBOL $trend %{T$WEATHER_FONT_IDX}$(get_icon "$forecast_icon")%{T-} $forecast_temp$SYMBOL"
 fi
