b1b2c6e8a8d7023b8e68b4b8b040b271f31e7627
Jean-Luc Tambasco <an.obscurity@gmail.com>
2018-09-13 01:13:15 +1000

    Fixed up rounding issue for weather app.

--- openweathermap-forecast.sh
+++ openweathermap-forecast.sh
@@ -25,19 +25,22 @@ get_icon() {
 }
 
 KEY="23a2c2a7dc0013ee24a45f4eae4bb548"
+#CITY="Collingwood,AU"
 CITY=""
 UNITS="metric"
 SYMBOL="°C"
 MATERIAL_FONT_IDX=2 # Index of the material icons font in the Polybar config file.
 WEATHER_FONT_IDX=3 # Index of the weather icons font in the Polybar config file.
 
+CITY=${CITY// /+}
+
 until wget -q -O - http://www.google.com | grep Lucky > /dev/null; do
     sleep 10
 done
 
 if [ ! -z $CITY ]; then
-    current=$(curl -sf "http://api.openweathermap.org/data/2.5/weather?APPID=$KEY&id=$CITY&units=$UNITS")
-    forecast=$(curl -sf "http://api.openweathermap.org/data/2.5/forecast?APPID=$KEY&id=$CITY&units=$UNITS&cnt=1")
+    current=$(curl -sf "http://api.openweathermap.org/data/2.5/weather?APPID=$KEY&q=$CITY&units=$UNITS")
+    forecast=$(curl -sf "http://api.openweathermap.org/data/2.5/forecast?APPID=$KEY&q=$CITY&units=$UNITS&cnt=1")
 else
     location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
 
@@ -51,15 +54,17 @@ else
 fi
 
 if [ ! -z "$current" ] && [ ! -z "$forecast" ]; then
-    current_temp=$(echo "$current" | jq ".main.temp" | cut -d "." -f 1)
+    current_temp=$(echo "$current" | jq ".main.temp")
+    current_temp=$(printf "%0.1f\n" $(echo "$current_temp+0.001" | bc) )
     current_icon=$(echo "$current" | jq -r ".weather[0].icon")
 
-    forecast_temp=$(echo "$forecast" | jq ".list[].main.temp" | cut -d "." -f 1)
+    forecast_temp=$(echo "$forecast" | jq ".list[].main.temp")
+    forecast_temp=$(printf "%0.1f\n" $(echo "$forecast_temp+0.0" | bc) )
     forecast_icon=$(echo "$forecast" | jq -r ".list[].weather[0].icon")
 
-    if [ "$current_temp" -gt "$forecast_temp" ]; then
+    if (( $(echo "$current_temp > $forecast_temp" | bc -l) )); then
         trend="%{T$MATERIAL_FONT_IDX}%{T-}"
-    elif [ "$forecast_temp" -gt "$current_temp" ]; then
+    elif (( $(echo "$forecast_temp > $current_temp" | bc -l) )); then
         trend="%{T$MATERIAL_FONT_IDX}%{T-}"
     else
         trend="%{T$MATERIAL_FONT_IDX}%{T-}"
