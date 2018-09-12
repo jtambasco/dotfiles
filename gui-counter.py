642e3e50d2b074f86db516f4452b053efda4e5f1
Jean-Luc Tambasco <an.obscurity@gmail.com>
2018-09-12 16:12:50 +1000

    Updated plugin and screenshot.

--- gui-counter.py
+++ gui-counter.py
@@ -4,72 +4,149 @@ import subprocess
 import sys
 from collections import Counter, OrderedDict
 
-# Program names and corresponding gylphs.
-programs = OrderedDict({
-    'termite': '',
-    'chrome': '',
+# Font from:
+# ttf-material-design-icons-git (https://materialdesignicons.com/)
+
+# Update GUIs
+GUI = True
+
+# Update processes
+PROCESS = True
+
+# Program/process names and corresponding gylphs.
+guis = OrderedDict({
+    'terminals': '',
+    'chromes': '',
     'firefox': '',
-    'klayout': '',
     'skypeforlinux': '',
-    'nemo': '蝹'
+    'filemanager': '蝹',
+    'remote-desktop': '',
+    'pdfviewer': '',
+    'image': '',
+    'klayout': '',
 })
 
-# Combine counts of program names in the tuple.  The resulting glpyh used will
-# be that of the first program name in the tuple.
-combine = [('chrome', 'chromium'),
-           ('nemo', 'dolphin', 'nautilus', 'pcmanfm')]
+processes = OrderedDict({
+    'vims': '',
+    'ssh': '',
+    'lumerical': '',
+    'updater': '練',
+})
 
+# Combine counts of program/process names in the tuple.  The resulting glpyh used will
+# be that of the corresponding key.
+combine_guis = {
+    'terminals': ('termite', 'terminator', 'urxvt'),
+    'chromes': ('chromium', 'chrome'),
+    'filemanger': ('nemo', 'thunar', 'dolphin', 'nautilus', 'pcmanfm'),
+    'remote-desktop': ('TeamViewer', ),
+    'pdfviewer': ('evince', 'okular', 'zathura'),
+    'image': ('gthumb', 'shotwell', 'deepin-image-vi'),
+}
 
-def get_running_guis():
-    # From https://askubuntu.com/questions/728157/how-to-get-the-list-of-running-gui-applications-in-the-unity-launcher
-    try:
-        listed = sys.argv[1]
-    except IndexError:
-        listed = []
+combine_proccesses = {
+    'vims': ('nvim', 'vim'),
+    'lumerical': ('fdtd-solutions', 'mode-solutions'),
+    'updater': ('pacman', 'yay', 'trizen', 'yaourt'),
+}
 
-    get = lambda cmd: subprocess.check_output(cmd).decode("utf-8").strip()
+################################################################################
 
-    def check_wtype(w_id):
-        # check the type of window; only list "NORMAL" windows
-        return "_NET_WM_WINDOW_TYPE_NORMAL" in get(["xprop", "-id", w_id])
+gui_output = ''
+process_output = ''
 
-    def get_process(w_id):
-        # get the name of the process, owning the window
-        proc = get(["ps", "-p", w_id, "-o", "comm="])
-        proc = "gnome-terminal" if "gnome-terminal" in proc else proc
-        return proc
+if GUI:
 
-    wlist = [l.split() for l in subprocess.check_output(["wmctrl", "-lp"])\
-             .decode("utf-8").splitlines()]
-    validprocs = [
-        get_process(w[2]) for w in wlist if check_wtype(w[0]) == True
-    ]
-
-    return validprocs
-
-
-# Get list of running GUI programs.
-running = Counter(get_running_guis())
-
-# Combine programs in combine list.
-for lst in combine:
-    count = 0
-    for i in lst[1:]:
+    def get_running_guis():
+        # From https://askubuntu.com/questions/728157/how-to-get-the-list-of-running-gui-applications-in-the-unity-launcher
         try:
-            count += running.pop(i)
-        except KeyError:
+            listed = sys.argv[1]
+        except IndexError:
+            listed = []
+
+        get = lambda cmd: subprocess.check_output(cmd).decode("utf-8").strip()
+
+        def check_wtype(w_id):
+            # check the type of window; only list "NORMAL" windows
+            return "_NET_WM_WINDOW_TYPE_NORMAL" in get(["xprop", "-id", w_id])
+
+        def get_process(w_id):
+            # get the name of the process, owning the window
+            proc = get(["ps", "-p", w_id, "-o", "comm="])
+            proc = "gnome-terminal" if "gnome-terminal" in proc else proc
+            return proc
+
+        wlist = [l.split() for l in subprocess.check_output(["wmctrl", "-lp"])\
+                 .decode("utf-8").splitlines()]
+        validprocs = [
+            get_process(w[2]) for w in wlist if check_wtype(w[0]) == True
+        ]
+
+        return validprocs
+
+    # Get list of running GUI programs.
+    gui_counts = Counter(get_running_guis())
+
+    # Combine programs in program combine list.
+    for k, lst in combine_guis.items():
+        count = 0
+        for i in lst:
+            try:
+                count += gui_counts.pop(i)
+            except KeyError:
+                pass
+        if count:
+            gui_counts[k] += count
+
+    # Generate program output.
+    for k, v in guis.items():
+        try:
+            c = gui_counts[k]
+            if c:
+                gui_output += '%s %i ' % (v, c)
+        except:
             pass
-    if count:
-        running[lst[0]] += count
 
-# Add counts to program list.
-for k, v in running.items():
-    programs[k] = (programs[k], v)
+if PROCESS:
 
-# Generate output.
-output = ''
-for v in programs.values():
-    if isinstance(v, tuple):
-        output += '%s %i ' % (v[0], v[1])
+    def get_running_proc(process_name_list):
+        counts = [None] * len(process_name_list)
 
-print(output)
+        for i, p in enumerate(process_name_list):
+            try:
+                count = int(
+                    subprocess.check_output(['pgrep', '-c', '-x',
+                                             p]).decode('utf-8'))
+            except subprocess.CalledProcessError:
+                count = 0
+            counts[i] = (p, count)
+
+        return dict(counts)
+
+    # Count running proccesses.
+    process_counts = get_running_proc(processes.keys())
+    combine_counts = get_running_proc(
+        list(sum(combine_proccesses.values(), ())))
+    process_counts.update(combine_counts)
+
+    # Combine processes in process combine list.
+    for k, lst in combine_proccesses.items():
+        count = 0
+        for i in lst:
+            try:
+                count += process_counts.pop(i)
+            except KeyError:
+                pass
+        if count:
+            process_counts[k] += count
+
+    # Generate process output.
+    for k, v in processes.items():
+        try:
+            c = process_counts[k]
+            if c:
+                process_output += '%s %i ' % (v, c)
+        except:
+            pass
+
+print(gui_output + process_output)
